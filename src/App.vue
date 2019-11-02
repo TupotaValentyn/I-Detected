215c269b41aca7af23716d3fcd2d874612d13a27
TupotaValentyn <gm.tupota.valik@gmail.com>
2019-11-02 16:03:40 +0200

    Get info from test endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,38 @@
 <template>
+  <div id="app">
+    <h1>Welcome to
+      <mark>I Detected</mark>
+      admin-panel
+    </h1>
+    <section>
+      <div v-bind="responseData">
 
-
-  <h1>Welcome to <mark>I Detected</mark> admin-panel</h1>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-  
+    import axios from 'axios';
+
+    export default {
+        name: 'app',
+        data: () => ({
+            responseData: [],
+            errors: []
+        }),
+
+        created() {
+            axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(response => {
+                    this.data = response;
+                })
+                .catch(e => {
+                    this.error.push(e);
+                })
+        }
+    }
+
 </script>
 
 <style>
